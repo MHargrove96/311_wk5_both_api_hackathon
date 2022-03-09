c1d1965a3b415380fcceebb96da5e9cb359384d7
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2022-03-08 20:06:59 -0600

    database set up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our server!");
+});
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}!`);
+});
